df03828b3c2f9d6aa256c4d3f1b47ffbcb1866c7
Tiffany-silva <silva.tiffd@gmail.com>
2023-11-29 03:58:00 +0530

    Update hashing to support selected values

--- src/Utils/hash.ts
+++ src/Utils/hash.ts
@@ -19,85 +19,119 @@ export const hashValue = (hashValue: string) => {
     return hashed;
 }
 
-export function hashCookies<T>(cookies: HarParam[]): HarParam[]{
-    for (var i in cookies) {
-        var hashed = hashValue(cookies[i].value);
-        cookies[i].value = "Hashed: " + hashed;
+export function hashCookies<T>(entryCookies: HarParam[], selectedCookies:Record<string, boolean>): HarParam[]{
+    for (let cookieName in entryCookies) {
+        Object.entries(selectedCookies).map(([item, status]: [string, boolean])=> {
+            if(entryCookies[cookieName].name == item && status) {
+                var hashed = hashValue(entryCookies[cookieName].value);
+                entryCookies[cookieName].value = "Hashed: " + hashed;
+            }
+        }) 
     }
-    return cookies;
+    return entryCookies;
 }
 
-export function hashHeaderCookies<T>(headers: HarParam[]): HarParam[] {
+export function hashHeaders<T>(entryHeaders: any, selectedHeaders:Record<string, boolean>): any[] {
 
-    for (let i in headers) {
-        if (headers[i].name == "Cookie" || headers[i].name == "Set-Cookie") {
-            const array = headers[i].value.split(';');
-            for (let i in array) {
-                const array1 = array[i].split('=');
-                array1[1] = hashValue(array1[1]);
-                array[i] = array1.join("=Hashed:");
-            }
-            const hashedValues = array.join(";");
-            headers[i].value = hashedValues;
-        } else if (headers[i].name == "Authorization") {
-            const authorizationValue = headers[i].value.split(" ");
-            const token = authorizationValue[1];
-          //  console.log(token);
-            if (token.includes(".")) {
-                authorizationValue[1] = token.substring(0, token.lastIndexOf("."));
-                //console.log(authorizationValue[1]);
-                headers[i].value = authorizationValue.join(" SignRemoved:");
-                //console.log(headers[i].value);
-            } else {
-                authorizationValue[1] = hashValue(token);
-                headers[i].value = authorizationValue.join(" Hashed:");
-            }
-        }
+    for (let headerName in entryHeaders) {
+            Object.entries(selectedHeaders).map(([item, status]: [string, boolean])=> {
+
+                if(entryHeaders[headerName].name == item && status) {
+                    if(entryHeaders[headerName].name=="Cookie" || entryHeaders[headerName].name=="Set-Cookie") {
+                        const array = entryHeaders[headerName].value.split(';');
+                        for (let i in array) {
+                            const array1 = array[i].split(/=(.*)/,2);
+                            array1[1] = hashValue(array1[1]);
+                            array[i] = array1.join("=Hashed:");
+                        }
+                        const hashedValues = array.join(";");
+                        entryHeaders[headerName].value = hashedValues;
+                    } 
+                    else if (entryHeaders[headerName].name == "Authorization") {
+                        const authorizationValue = entryHeaders[headerName].value.split(" ");
+                        const token = authorizationValue[1];
+                      //  console.log(token);
+                        if (token.includes(".")) {
+                            authorizationValue[1] = token.substring(0, token.lastIndexOf("."));
+                            //console.log(authorizationValue[1]);
+                            entryHeaders[headerName].value = authorizationValue.join(" SignRemoved:");
+                            //console.log(headers[i].value);
+                        } else {
+                            authorizationValue[1] = hashValue(token);
+                            entryHeaders[headerName].value = authorizationValue.join(" Hashed:");
+                        }
+                    }
+                }
+                
+               
+            })
+        
+        
     }
-    return headers;
+    return entryHeaders;
 };
 
-export function hashPostQueryParams(postDataParams: HarParam[]): HarParam[] {
+export function hashPostQueryParams(postDataParams: HarParam[],  selectedPostData:Record<string, boolean>): HarParam[] {
 
     //var postDataParams = harcontent.log.entries[63].request.postData.params;
-    for (let i in postDataParams) {
-        if (postDataParams[i].name == "code") {
-            postDataParams[i].value = " Hashed:" + hashValue(postDataParams[i].value);
-        } else if (postDataParams[i].name == "SAMLRequest") {
-            const signatureRemovedSamlAssertion=getSignatureRemovedSamlAssertion(postDataParams[i].value).toString();
-            postDataParams[i].value = "signatureValueRemoved:" + signatureRemovedSamlAssertion;
-        }
+    for (let postDataValue in postDataParams) {
+        Object.entries(selectedPostData).map(([item, status]: [string, boolean])=> {
+
+            if(postDataParams[postDataValue].name == item && status) {
+            
+                if (postDataParams[postDataValue].name == "code") {
+                    postDataParams[postDataValue].value = " Hashed:" + hashValue(postDataParams[postDataValue].value);
+                } else if (postDataParams[postDataValue].name == "SAMLRequest") {
+                    const signatureRemovedSamlAssertion=getSignatureRemovedSamlAssertion(postDataParams[postDataValue].value).toString();
+                    postDataParams[postDataValue].value = "signatureValueRemoved:" + signatureRemovedSamlAssertion;
+                }
+            }
+            
+        })
+       
     }
     return postDataParams;
 }
 
-export function hashQueryStringparams<T>(queryStringParams: HarParam[]): HarParam[] {
+export function hashQueryStringparams<T>(queryStringParams: HarParam[], selectedQueryStringParams:Record<string, boolean>): HarParam[] {
 
-    for (let i in queryStringParams) {
-        if (queryStringParams[i].name == "code") {
-            queryStringParams[i].value = " Hashed: " + hashValue(queryStringParams[i].value);
-        } else if(queryStringParams[i].name == "id_token_hint") {
-            queryStringParams[i].value = "Signature Removed:" + queryStringParams[i].value.substring(0, queryStringParams[i].value.lastIndexOf("."));
-        }   
+    for (let queryStringParamName in queryStringParams) {
+        Object.entries(selectedQueryStringParams).map(([item, status]: [string, boolean])=> {
+
+            if(queryStringParams[queryStringParamName].name == item && status) {
+                if (queryStringParams[queryStringParamName].name == "code") {
+                    queryStringParams[queryStringParamName].value = " Hashed: " + hashValue(queryStringParams[queryStringParamName].value);
+                } else if(queryStringParams[queryStringParamName].name == "id_token_hint") {
+                    queryStringParams[queryStringParamName].value = "Signature Removed:" + 
+                    queryStringParams[queryStringParamName].value.substring(0, queryStringParams[queryStringParamName].value.lastIndexOf("."));
+                }
+            }
+        })
+           
     }
     return queryStringParams;
 }
 
-export const hashIdToken = (idToken: string) => {
+export const hashIdToken = (idToken: string, selectedPostData:Record<string, boolean>) => {
 
     var baseurl = idToken.substring(0, idToken.indexOf("?"));
     var urlparams = idToken.substring(idToken.indexOf("?") + 1, idToken.length).split("&");
-   // console.log(urlparams);
-    for (let i in urlparams) {
-        var props = urlparams[i].split("=");
-        if (props[0] == "id_token_hint") {
-            const token = props[1];
-           // console.log(token);
-            if (token.includes(".")) {
-                props[1] = "Signature Removed:" + token.substring(0, token.lastIndexOf("."));
+    for (let urlparam in urlparams) {
+        let props = urlparams[urlparam].split("=");
+        Object.entries(selectedPostData).map(([item, status]: [string, boolean])=> {
+            if(props[0] == item && status) {
+                if (props[0] == "id_token_hint") {
+                    const token = props[1];
+                   // console.log(token);
+                    if (token.includes(".")) {
+                        props[1] = "Signature Removed:" + token.substring(0, token.lastIndexOf("."));
+                    }
+                }
+                urlparams[urlparam] = props.join("=");
+                idToken = baseurl + "?" + (urlparams.join("&"));
             }
-        }
-        urlparams[i] = props.join("=");
-        return idToken = baseurl + "?" + (urlparams.join("&"));
+        })
+        return idToken;
+        
     }
 }

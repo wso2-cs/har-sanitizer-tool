03fe432bee4c9cd8f808c552c2853c5c2cada2d1
Tiffany-silva <silva.tiffd@gmail.com>
2023-11-29 03:57:19 +0530

    Add functionality to Sanitize List

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 import "./App.css";
 import { useState } from "react";
-import { FileInput, Label, Button } from "flowbite-react";
+import { FileInput } from "flowbite-react";
 import * as HashUtils from "./Utils/hash";
 import { removeSensitiveInfo } from "./Utils/removeInfo";
 import { HarParam } from "./Utils/hash";
 import { SanitizeSelector } from "./SanitizingSelector";
 
-export type SanitizeState = Record<string, string[]>;
+export type SanitizeState = Record<string, Record<string,boolean>>;
+export const defaultSensitiveInfoList = [
+	"Authorization",
+	"SAMLRequest",
+	"SAMLResponse",
+	"access_token",
+	"appID",
+	"assertion",
+	"code",
+	"email",
+	"id_token",
+	"password",
+  "id_token_hint",
+  "commonAuthId",
+  "opbs",
+  "JSESSIONID",
+  "Set-Cookie",
+  "Cookie"
+
+];
+
+export const defaultSelectedSanitizeList = [ ...defaultSensitiveInfoList];
 
 function App() {
   const fileReader = new FileReader();
   const [jsonHar, setJsonHar] = useState<string>();
+  const [downloadJsonHar, setdownloadJsonHarJsonHar] = useState<any>();
+
   const [harError, setHarError] = useState<string>("");
 
+  
   const defaultSanitizeList: SanitizeState = {
-    cookies: [] as string[],
-    headers: [] as string[],
-    postData: [] as string[],
-    queryStringParams: [] as string[],
+    cookies: {},
+    headers: {},
+    postData: {},
+    queryStringParams: {},
   };
 
   const [sanitizeList, setSanitizeList] =
@@ -37,9 +61,8 @@ function App() {
         (typeof content === "string" || content instanceof String)
       ) {
         var harcontent = JSON.parse(content as string);
-        var sanitizedHarContent;
-        sanitizedHarContent = sanitizeSensitiveInfo(harcontent);
-        console.log(sanitizedHarContent);
+        setdownloadJsonHarJsonHar(harcontent);
+        setSanitizeList(getSanitizeItems(harcontent));
         setJsonHar(JSON.stringify(harcontent));
         setHarError("");
         return;
@@ -51,136 +74,117 @@ function App() {
     }
   };
 
-  function addToUniqueSanitizeList(
-    cookies: any[],
-    headers: any[],
-  ) {
-    Object.keys(sanitizeList).forEach((key) => {
-      if (key == "cookies" && cookies) {
-        addToUniqueArray(cookies, key);
-      } else if(key == "headers" && headers) {
-        addToUniqueArray(headers, key);
-      } 
-    });
-    console.log(sanitizeList);
-  }
+  function getHarParamsArr(harcontent:any) {
 
-  function addToUniqueSanitizeListPostData(
-    postData: any[],
-  ) {
-    Object.keys(sanitizeList).forEach((key) => {
-      if (key == "postData" && postData) {
-        addToUniqueArray(postData, key);
-      }
-    });
-  }
+    const harParamsArr = {
+      headers: new Set<string>(),
+      queryStringParams: new Set<string>(),
+      cookies: new Set<string>(),
+      postData: new Set<string>(),
+    };
 
-
-  function addToUniqueSanitizeListQueryParams(
-    queryStringParams: any[],
-  ) {
-    Object.keys(sanitizeList).forEach((key) => {
-      if (key == "queryStringParams" && queryStringParams) {
-        addToUniqueArray(queryStringParams, key);
-      }
-    });
-  }
-  
-  function addToUniqueArray(paramList:any[], key:string) {
-    for (let i in paramList) {
-      if (!sanitizeList[key].includes(paramList[i].name)) {
-        sanitizeList[key].push(paramList[i].name);
-      }
-    }
-  }
-
-  const sanitizeSensitiveInfo = (harcontent: any) => {
     const entries = harcontent.log.entries;
     const pages = harcontent.log.pages;
     for (let i in entries) {
       let request = entries[i].request;
-      addToUniqueSanitizeList(request.cookies, request.headers);
-      const sanitizedReqHeaderCookies = HashUtils.hashHeaderCookies(
-        request.headers
+      request.headers.map((header: HarParam) => harParamsArr.headers.add(header.name));
+      request.cookies.map((cookie: HarParam) => harParamsArr.cookies.add(cookie.name));
+      if (request.postData) {
+        request.postData.params.map((postData: HarParam) => harParamsArr.postData.add(postData.name));
+      }
+      if (request.queryString.length != 0) {
+        request.queryString.map((queryString: HarParam) => harParamsArr.queryStringParams.add(queryString.name));
+      }
+
+      let response = entries[i].response;
+      response.headers.map((header: HarParam) => harParamsArr.headers.add(header.name));
+		  response.cookies.map((cookie: HarParam) => harParamsArr.cookies.add(cookie.name));     
+    }
+
+    return {
+      headers: [...harParamsArr.headers].sort(),
+      queryStringParams: [...harParamsArr.queryStringParams].sort(),
+      cookies: [...harParamsArr.cookies].sort(),
+      postData: [...harParamsArr.postData].sort(),
+    };
+  }
+
+  function getSanitizeItems(harcontent: string): SanitizeState {
+    const sanitizeItems = getHarParamsArr(harcontent);
+    const output = { ...defaultSanitizeList };
+    Object.entries(sanitizeItems).map(([key, items]: [string, string[]]) => {
+      output[key] = items.reduce(
+        (acc, curr) => {
+          if (!curr) return acc;
+          acc[curr] = defaultSelectedSanitizeList.includes(curr);
+          return acc;
+        },
+        {} as Record<string, boolean>,
       );
-      const sanitizedReqCookies = HashUtils.hashCookies(request.cookies);
-      request.headers = sanitizedReqHeaderCookies;
+    });
+    console.log(output);
+    return output;
+  }
+  
+
+  const sanitizeSensitiveInfo = (harcontent: any) => {
+    let sanitizedOutput = getSanitizeItems(harcontent);
+    console.log(sanitizedOutput);
+    const entries = harcontent.log.entries;
+    const pages = harcontent.log.pages;
+    for(let entry in entries) {
+      let request = entries[entry].request;
+      console.log(request.cookies);
+      const sanitizedReqHeaders = HashUtils.hashHeaders(request.headers, sanitizeList.headers);
+      const sanitizedReqCookies = HashUtils.hashCookies(request.cookies, sanitizeList.cookies);
+      request.headers = sanitizedReqHeaders;
       request.cookies = sanitizedReqCookies;
       if (request.postData) {
-        console.log(request.postData.toString());
-         addToUniqueSanitizeListPostData(request.postData.params);
-        const sanitizedReqpostDataText = removeSensitiveInfo(
-          request.postData.text
-        );
-        const sanitizedReqPostDataParams = HashUtils.hashPostQueryParams(
-          request.postData.params
-        );
+        const sanitizedReqpostDataText = removeSensitiveInfo(request.postData.text,sanitizeList.postData);
+        const sanitizedReqPostDataParams = HashUtils.hashPostQueryParams(request.postData.params, sanitizeList.postData);
         request.postData.text = sanitizedReqpostDataText;
         request.postData.params = sanitizedReqPostDataParams;
       }
+
       if (request.queryString.length != 0) {
-        addToUniqueSanitizeListQueryParams(request.queryString);
-        const sanitizedQueryStringCodeParam = HashUtils.hashQueryStringparams(
-          request.queryString
+      
+         const sanitizedQueryStringCodeParam = HashUtils.hashQueryStringparams(
+          request.queryString, sanitizeList.queryStringParams
         );
         request.queryString = sanitizedQueryStringCodeParam;
       }
 
       if (request.url.includes("id_token_hint")) {
-        const sanitizedReqUrlWithIDToken = HashUtils.hashIdToken(request.url);
+        const sanitizedReqUrlWithIDToken = HashUtils.hashIdToken(request.url, sanitizeList.queryStringParams);
         request.url = sanitizedReqUrlWithIDToken;
       }
-
-      let response = entries[i].response;
-      addToUniqueSanitizeList(response.cookies, response.headers);
-      const sanitizedRespHeaderCookies = HashUtils.hashHeaderCookies(
-        response.headers
+      let response = entries[entry].response;
+      const sanitizedRespHeaders = HashUtils.hashHeaders(
+        response.headers, sanitizeList.headers
       );
-      const sanitizedRespCookies = HashUtils.hashCookies(response.cookies);
-      response.headers = sanitizedRespHeaderCookies;
+      const sanitizedRespCookies = HashUtils.hashCookies(response.cookies, sanitizeList.cookies);
+      response.headers = sanitizedRespHeaders;
       response.cookies = sanitizedRespCookies;
     }
-
-    for (let i in pages) {
+     for (let i in pages) {
       if (pages[i].title.includes("id_token_hint")) {
-        const sanitizedIDtokenPageTitle = HashUtils.hashIdToken(pages[i].title);
+        const sanitizedIDtokenPageTitle = HashUtils.hashIdToken(pages[i].title, sanitizeList.queryStringParams);
         pages[i].title = sanitizedIDtokenPageTitle;
       }
     }
-    console.log(sanitizeList);
 
     harcontent.log.entries = entries;
     harcontent.log.pages = pages;
+    setdownloadJsonHarJsonHar(harcontent);
     return harcontent;
   };
 
-  // // This function will be triggered when a checkbox changes its state
-  // const selectSanitizeItem = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const selectedValue = event.target.value;
-
-  //   // Check if "ids" contains "selectedIds"
-  //   // If true, this checkbox is already checked
-  //   // Otherwise, it is not selected yet
-  //   if (selectedSanitizeList.cookies.includes(selectedValue)) {
-  //     const selectedValues = selectedSanitizeList.cookies.filter(
-  //       (value) => value !== selectedValue
-  //     );
-  //     selectedSanitizeList.cookies = selectedValues;
-  //   } else {
-  //     const newSel = [...selectedSanitizeList.cookies];
-  //     newSel.push(selectedValue);
-  //     selectedSanitizeList.cookies = newSel;
-  //   }
-  // };
-
-  const getCookieValuesUnique = () => {
-    console.log(sanitizeList.cookies.values());
-    return sanitizeList.cookies;
-  };
-
   const downloadTxtFile = () => {
+    var sanitizedHarContent;
+    sanitizeSensitiveInfo(downloadJsonHar);
+    sanitizedHarContent = JSON.stringify(downloadJsonHar);
     const element = document.createElement("a");
-    const file = new Blob([jsonHar as string], { type: "application/json" });
+    const file = new Blob([sanitizedHarContent as string], { type: "application/json" });
     element.href = URL.createObjectURL(file);
     element.download = "myFile.har";
     document.body.appendChild(element);
@@ -221,7 +225,9 @@ function App() {
           {jsonHar ? (
             <button
               type="button"
-              className="object-right text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              className="object-right text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
+              focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 
+              dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
               onClick={(e) => downloadTxtFile()}
             >
               Download

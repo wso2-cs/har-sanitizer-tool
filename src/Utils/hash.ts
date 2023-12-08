64f4701738fc23fb37e21f6cf75f8e83cc2d704d
Tiffany-silva <silva.tiffd@gmail.com>
2023-12-08 20:45:31 +0530

    Update hashing and removal feature

--- src/Utils/hash.ts
+++ src/Utils/hash.ts
@@ -1,5 +1,6 @@
 import sha256 from "fast-sha256";
 import { getSignatureRemovedSamlAssertion } from "./decoder";
+import { SanitizeState } from "../App";
 
 export interface HarParam {
 	name: string;
@@ -9,9 +10,7 @@ export interface HarParam {
 export const hashValue = (hashValue: string) => {
 	const encoder = new TextEncoder();
 	const msgUint8 = encoder.encode(hashValue);
-
 	const sha256HashedValue = sha256(msgUint8);
-
 	const hashArray = Array.from(new Uint8Array(sha256HashedValue));
 	const hashed = hashArray.map((b) => b.toString(24).padStart(2, "3")).join(""); // convert bytes to hex string
 	return hashed;
@@ -34,11 +33,12 @@ export function hashCookies<T>(
 
 export function hashRemoveHeaders<T>(
 	entryHeaders: any,
-	selectedHeaders: Record<string, boolean>,
-	hashEnabled: boolean
+	selectedValues: SanitizeState,
+	hashEnabled: boolean,
+	removalEnabled: boolean
 ): any[] {
 	for (let headerName in entryHeaders) {
-		Object.entries(selectedHeaders).map(([item, status]: [string, boolean]) => {
+		Object.entries(selectedValues.headers.sanitizeList).map(([item, status]: [string, boolean]) => {
 			if (entryHeaders[headerName].name == item && status) {
 				if (
 					entryHeaders[headerName].name == "Cookie" ||
@@ -55,24 +55,26 @@ export function hashRemoveHeaders<T>(
 				} else if (entryHeaders[headerName].name == "Authorization") {
 					const authorizationValue = entryHeaders[headerName].value.split(" ");
 					const token = authorizationValue[1];
-					//  console.log(token);
 					if (token.includes(".")) {
 						authorizationValue[1] = token.substring(0, token.lastIndexOf("."));
-						//console.log(authorizationValue[1]);
 						entryHeaders[headerName].value =
 							authorizationValue.join(" SignRemoved:");
-						//console.log(headers[i].value);
 					} else {
 						authorizationValue[1] = hashValue(token);
 						entryHeaders[headerName].value =
 							authorizationValue.join(" Hashed:");
 					}
+				} else if (entryHeaders[headerName].name == "Location" || "Referer") {
+					entryHeaders[headerName].value = hashRemoveUrlParams(entryHeaders[headerName].value,
+						selectedValues.queryStringParams.sanitizeList,
+						selectedValues.queryStringParams.sanitizeOption.hashEnabled,
+						selectedValues.queryStringParams.sanitizeOption.removalEnabled);
 				}
-				if (hashEnabled) {
+				if (hashEnabled && (entryHeaders[headerName].name != "Cookie" || "Set-Cookie" || "Authorization" || "Location")) {
 					const hashedValue = hashValue(entryHeaders[headerName].value);
 					entryHeaders[headerName].value = "Hashed:" + hashedValue;
 
-				} else {
+				} else if (removalEnabled && (entryHeaders[headerName].name != "Cookie" || "Set-Cookie" || "Authorization" || "Location")) {
 					entryHeaders[headerName].value = "[HAR_SANITIZER:REMOVED]";
 				}
 			}
@@ -84,9 +86,9 @@ export function hashRemoveHeaders<T>(
 export function hashRemovePostQueryParams(
 	postDataParams: HarParam[],
 	selectedPostData: Record<string, boolean>,
-	isHashEnabled: boolean
+	isHashEnabled: boolean,
+	isRemovalEnabled: boolean
 ): HarParam[] {
-	//var postDataParams = harcontent.log.entries[63].request.postData.params;
 	for (let postDataValue in postDataParams) {
 		Object.entries(selectedPostData).map(
 			([item, status]: [string, boolean]) => {
@@ -101,9 +103,12 @@ export function hashRemovePostQueryParams(
 							).toString();
 						postDataParams[postDataValue].value =
 							"signatureValueRemoved:" + signatureRemovedSamlAssertion;
-					} else if (isHashEnabled) {
+					} else if (isHashEnabled && postDataParams[postDataValue].name != "code" || "SAMLRequest") {
 						postDataParams[postDataValue].value =
 							" Hashed:" + hashValue(postDataParams[postDataValue].value);
+					} else if (isRemovalEnabled && postDataParams[postDataValue].name != "code" || "SAMLRequest") {
+						postDataParams[postDataValue].value = "[HAR_SANITIZER:REMOVED]"
+						console.log(postDataParams[postDataValue].value);
 					}
 				}
 			}
@@ -115,7 +120,8 @@ export function hashRemovePostQueryParams(
 export function hashRemoveQueryStringparams<T>(
 	queryStringParams: HarParam[],
 	selectedQueryStringParams: Record<string, boolean>,
-	isHashEnabled: boolean
+	isHashEnabled: boolean,
+	isRemovalEnabled: boolean
 ): HarParam[] {
 	for (let queryStringParamName in queryStringParams) {
 		Object.entries(selectedQueryStringParams).map(
@@ -134,12 +140,16 @@ export function hashRemoveQueryStringparams<T>(
 								0,
 								queryStringParams[queryStringParamName].value.lastIndexOf(".")
 							);
-					} else if (isHashEnabled) {
+					}
+					if (isHashEnabled && (queryStringParams[queryStringParamName].name != "code" || "id_token_hint")) {
 						queryStringParams[queryStringParamName].value =
 							" Hashed: " +
 							hashValue(queryStringParams[queryStringParamName].value);
-					} else {
+						console.log(queryStringParams[queryStringParamName].value);
+					} else if (isRemovalEnabled && (queryStringParams[queryStringParamName].name != "code" || "id_token_hint")) {
 						queryStringParams[queryStringParamName].value = "[HAR_SANITIZER:REMOVED]"
+						console.log(queryStringParams[queryStringParamName].value);
+
 					}
 				}
 			}
@@ -150,28 +160,35 @@ export function hashRemoveQueryStringparams<T>(
 
 export const hashRemoveUrlParams = (
 	url: string,
-	selectedPostData: Record<string, boolean>,
-	isHashEnabled: boolean
+	selectedQueryStringParams: Record<string, boolean>,
+	isHashEnabled: boolean,
+	isRemovalEnabled: boolean
 ) => {
+	console.log(isHashEnabled, isRemovalEnabled);
 	var baseurl = url.substring(0, url.indexOf("?"));
 	var urlparams = url
 		.substring(url.indexOf("?") + 1, url.length)
 		.split("&");
 	for (let urlparam in urlparams) {
 		let props = urlparams[urlparam].split("=");
-		Object.entries(selectedPostData).map(
+		Object.entries(selectedQueryStringParams).map(
 			([item, status]: [string, boolean]) => {
 				if (props[0] == item && status) {
 					if (props[0] == "id_token_hint") {
 						const token = props[1];
-						// console.log(token);
 						if (token.includes(".")) {
 							props[1] =
 								"Signature Removed:" +
 								token.substring(0, token.lastIndexOf("."));
 						}
-					} else if (isHashEnabled) {
+					}
+					else if (props[0] == "code") {
 						props[1] = "Hashed:" + hashValue(props[1]);
+					}
+					if (isHashEnabled && (props[0] != "id_token_hint" || "code")) {
+						props[1] = "Hashed:" + hashValue(props[1]);
+					} else if (isRemovalEnabled && (props[0] != "id_token_hint" || "code")) {
+						props[1] = "[HAR_SANITIZER:REMOVED]";
 					}
 					urlparams[urlparam] = props.join("=");
 				}

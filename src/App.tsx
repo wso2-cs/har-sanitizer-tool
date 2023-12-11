a614f5c5f17910b0ea40c6d54801c63b84fdb167
Tiffany-silva <silva.tiffd@gmail.com>
2023-12-11 20:19:31 +0530

    Update footer bg colour

--- src/App.tsx
+++ src/App.tsx
@@ -322,10 +322,10 @@ function App() {
 				jsonHar={jsonHar}
 			></SanitizeSelector>
 
-			<footer className="bg-black dark:bg-gray-900">
+			<footer className="bg-white dark:bg-gray-900">
 				<div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-					<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-					<span className="block text-sm text-white sm:text-center dark:text-white-400">© 2023 <a href=""
+					<hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+					<span className="block text-sm text-gray sm:text-center dark:text-gray-400">© 2023 <a href=""
 					className="hover:underline"></a>All Rights Reserved</span>
 				</div>
 			</footer>
